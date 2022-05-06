2926689c1949377cda81a9d395c72ea319193f72
fedepared <federico.pared@gmail.com>
2022-05-06 14:39:48 -0300

    fix navMenu no se muestra si no estan logueados

--- client/src/app/guardian.guard.ts
+++ client/src/app/guardian.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
@@ -8,19 +9,24 @@ import { AuthService } from './services/auth.service';
 })
 export class GuardianGuard implements CanActivate {
   isLoggedIn$: Observable<boolean>;
-  constructor(private authService:AuthService, private router:Router){
+  isLoggedIn:boolean;
+  constructor(private authService:AuthService, private router:Router,public _matSnackBar:MatSnackBar){
     this.isLoggedIn$=this.authService.checkSessionStorage();
+    this.isLoggedIn$.subscribe(state => this.isLoggedIn=state);
   }
   
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     
-    // if(!this.isLoggedIn$)
-    // {
-    //   console.log("No estas logueado");
-    //   this.router.navigate(['login']);
-    // }
+      if(!this.isLoggedIn)
+      {
+        console.log("No estas logueado");
+        this.router.navigate(['login']);
+        this._matSnackBar.open("Debe iniciar sesión","Error!",{
+          duration:3000
+        })
+      }
     // if(localStorage.getItem('isOp') === 'true')
     // {
     //   this.router.navigate(['procesos'])
