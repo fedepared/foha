c5f19b5e54a3e3953b9af30008ad51a2c649d532
Christian <castronicolasalan@gmail.com>
2022-06-01 19:21:58 -0300

    Algo en Etapa para Merge

--- Controllers/EtapaController.cs
+++ Controllers/EtapaController.cs
@@ -675,7 +675,7 @@ namespace Foha.Controllers
             if(etapaAntes.InicioProceso==null)
             {
                 //Tiempo parcial 
-                TimeSpan preEtapaTiempoParc = (DateTime.Now - DateTime.Parse(etapaAntes.TiempoParc));
+                TimeSpan preEtapaTiempoParc = (DateTime.Now - DateTime.ParseExact(etapaAntes.TiempoParc, "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
                 //SOLO PARA ETAPA HORNO
                 if(editEtapaDto.IdTipoEtapa == 20)
                 {
