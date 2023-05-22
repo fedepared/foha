ad5821b545b62a3e12c896d3ee0174d3fdf3cf43
Christian <castronicolasalan@gmail.com>
2023-05-22 11:33:00 -0300

    Fix CheckearEtapas 5

--- Controllers/TransformadoresController.cs
+++ Controllers/TransformadoresController.cs
@@ -1785,9 +1785,9 @@ namespace Foha.Controllers
     }
 
     private string ChequearFechasProd(){
-        List<Etapa> etapas = _context.Etapa.Where(x => (x.IdTipoEtapa == 17 || x.IdTipoEtapa == 34) && (x.IdColor == null || x.IdColor == 1047) && x.IdTransfoNavigation.FechaProd > DateTime.Today).OrderBy(x => x.IdTransfoNavigation.FechaProd).Include(x => x.IdTransfoNavigation).ToList();
+        List<Etapa> etapas = _context.Etapa.Where(x => (x.IdTipoEtapa == 17 || x.IdTipoEtapa == 34) && (x.IdColor == null || x.IdColor == 1047)).OrderBy(x => x.IdTransfoNavigation.FechaProd).Include(x => x.IdTransfoNavigation).ToList();
         foreach(Etapa e in etapas){
-            if((e.IdTransfoNavigation.FechaProd.Value - DateTime.Today).Days <= 25 )
+            if((e.IdTransfoNavigation.FechaProd.Value - DateTime.Today).Days <= 25 && (e.IdTransfoNavigation.FechaProd.Value - DateTime.Today).Days >=0  )
             {
                 e.IdColor = 1047;
             }
@@ -1808,7 +1808,7 @@ namespace Foha.Controllers
     }
 
     [HttpGet("ChequearFechasProd")]
-    public async Task<IActionResult> ChequearFechasProdGet([FromRoute] int mes, [FromRoute] int anio){
+    public async Task<IActionResult> ChequearFechasProdGet(){
         Response<String> r = new Response<String>();
         try{
             ChequearFechasProd();
